184819b3a76d4af14889f6924f6be693072cb591
gabriel <furstenheim@gmail.com>
2017-09-14 19:25:47 +0200

    Highlight code

--- presentation/app.js
+++ presentation/app.js
@@ -1,17 +1,25 @@
 require('reveal.js/lib/js/head.min.js')
 const Reveal = require('reveal.js')
 window.Reveal = Reveal
+const hljs = require('reveal.js/plugin/highlight/highlight-origin.js')
+require('reveal.js/plugin/highlight/highlight.js')
+// Reveal assumes this is global
+window.hljs = hljs
 Reveal.initialize({
+  postMessage: true,
   dependencies: [
     { src: 'node_modules/reveal.js/plugin/markdown/marked.js' },
     { src: 'node_modules/reveal.js/plugin/markdown/markdown.js' },
-    { src: 'node_modules/reveal.js/plugin/notes/notes.js', async: true }/*,
+    { src: 'node_modules/reveal.js/plugin/notes/notes.js', async: true },
     {
       src: 'plugin/highlight/highlight.js',
       async: true,
       callback: function () {
-        window.hljs.initHighlightingOnLoad()
+        hljs.initHighlightingOnLoad()
       }
-    }*/
+    }
   ]
+})
+Reveal.addEventListener('ready', function () {
+  Reveal.slide(3)
 })
