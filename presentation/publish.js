bef99ea8834ce2531730d5ac54aa4bd29861d976
furstenheim <furstenheim@gmail.com>
2017-10-12 18:31:27 +0200

    Fix paths

--- presentation/publish.js
+++ presentation/publish.js
@@ -1,5 +1,6 @@
 const ghPages = require('gh-pages')
-ghPages.publish('.', {
-  src: ['bundle.js', 'node_modules/reveal.js/**.*', 'node_modules/font-awesome/**.*', 'index.html', 'resources/**.*'],
-  dest: '..'
+ghPages.publish('./', {
+  src: ['bundle.js', 'node_modules/reveal.js/**.*', 'node_modules/font-awesome/**.*', 'index.html', 'resources/**.*']
+}, function (err, a) {
+  console.log(err, a)
 })
