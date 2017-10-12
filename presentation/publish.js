b6b69567c5147837d85856777a670251e9c8178a
furstenheim <furstenheim@gmail.com>
2017-10-12 18:39:30 +0200

    Add all css

--- presentation/publish.js
+++ presentation/publish.js
@@ -1,6 +1,6 @@
 const ghPages = require('gh-pages')
 ghPages.publish('./', {
-  src: ['bundle.js', 'node_modules/reveal.js/**/**.*', 'node_modules/font-awesome/**/**.*', 'index.html', 'resources/**.*']
+  src: ['bundle.js', 'node_modules/reveal.js/**/**.*', 'node_modules/font-awesome/**/**.*', 'index.html', 'resources/**/**.*']
 }, function (err) {
   if (err) {
     console.log(err)
