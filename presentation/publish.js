42942059db25b02004debf7e991311a47d77e7b2
furstenheim <furstenheim@gmail.com>
2017-10-12 18:28:56 +0200

    Add publish to gh-pages

---
+++ presentation/publish.js
@@ -0,0 +1,5 @@
+const ghPages = require('gh-pages')
+ghPages.publish('.', {
+  src: ['bundle.js', 'node_modules/reveal.js/**.*', 'node_modules/font-awesome/**.*', 'index.html', 'resources/**.*'],
+  dest: '..'
+})
